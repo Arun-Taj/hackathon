c6904c7b770e6057197cd72273f985210f3c7570
Arun-Taj <aruntajpuriya1@gmail.com>
2023-09-03 02:40:40 -0600

    rearranged indexpage

--- health/myapp/urls.py
+++ health/myapp/urls.py
@@ -6,4 +6,8 @@ from . import views
 urlpatterns = [
  
     path('',views.index,name='index' ),
+    path('about_us/',views.about_us,name='about_us'),
+    path('register/',views.register,name='register'),
+    path('sign_in/',views.sign_in,name='sign_in'),
+
 ]
--- health/myapp/views.py
+++ health/myapp/views.py
@@ -4,3 +4,34 @@ from django.shortcuts import render
 
 def index(request):
     return render(request,'index.html')
+
+
+
+def about_us(request):
+    return render(request,'about.html')
+
+
+
+
+def register(request):
+    if request.method == 'POST' :
+        print(request.POST)
+        user_name=request.POST['name']
+        print(user_name)
+        password=request.POST['pswrd']
+        conf_password=request.POST['conf_pswrd']
+        img=request.FILES['filename']
+        user_create=Registerform(user_name=user_name)
+        user_create.save()
+        print('hello')
+        print(img)
+        # form=form_register(user_name,password,conf_password)
+        
+        # return redirect('home')
+        
+    return render(request,'register.html')
+
+
+
+def sign_in(request):
+    return render(request,'login.html')
